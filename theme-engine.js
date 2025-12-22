672837a3259d321b6f2e008d3dd0451af8522bd5
jamalkatab69-cell <jamalkatab69@gmail.com>
2025-12-22 10:25:27 +0100

    Delete theme-engine.js

--- theme-engine.js
+++
@@ -1,9 +0,0 @@
-export const ThemeEngine = {
-    init() {
-        // تطبيق الثيم الداكن افتراضياً
-        document.body.classList.add('dark-mode');
-    },
-    toggle() {
-        document.body.classList.toggle('light-mode');
-    }
-};
