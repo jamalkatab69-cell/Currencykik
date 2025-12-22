997063c07a1848ea5c935d0e55b5a9220914da17
jamalkatab69-cell <jamalkatab69@gmail.com>
2025-12-22 10:42:38 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 export const CONFIG = {
-  API_KEY: "YOUR_TWELVEDATA_API_KEY",
-  REFRESH_MS: 20 * 60 * 1000, // تحديث كل 20 دقيقة
+  API_KEY: "b83fce53976843bbb59336c03f9a6a30",
+  REFRESH_MS: 20 * 60 * 1000, // كل 20 دقيقة
   ICON_BASE: "https://raw.githubusercontent.com/jamalkatabeuro-sketch/My-website/main/",
 };
