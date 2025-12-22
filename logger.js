77e9624b2c99fac0d4f4833c085908f71c2ba397
jamalkatab69-cell <jamalkatab69@gmail.com>
2025-12-22 10:26:41 +0100

    Delete logger.js

--- logger.js
+++
@@ -1,4 +0,0 @@
-export const Logger = {
-    log(msg) { console.log(`[INFO] ${new Date().toLocaleTimeString()}: ${msg}`); },
-    error(msg) { console.error(`[ERROR] ${new Date().toLocaleTimeString()}: ${msg}`); }
-};
