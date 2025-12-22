2c73cdeb35ddc1bcd5105847f5f282221bed551e
jamalkatab69-cell <jamalkatab69@gmail.com>
2025-12-22 09:30:58 +0100

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,28 @@
+export const CONFIG = {
+    API_KEY: 'b83fce53976843bbb59336c03f9a6a30',
+    BASE_URL: 'https://api.twelvedata.com',
+    IMG_URL: 'https://raw.githubusercontent.com/jamalkatabeuro-sketch/My-website/main/',
+    CACHE_TIME: 3600000, // ساعة واحدة بالملي ثانية
+    CURRENCIES: {
+        "USD": { flag: "101-currency-usd.png", icon: "101-currency-usd.png" }, // لم تذكر USX، استخدمت الأصلية
+        "EUR": { flag: "100-currency-eur.png", icon: "100-currency-eurx.png" },
+        "GBP": { flag: "102-currency-gbp.png", icon: "102-currency-gbpx.png" },
+        "CHF": { flag: "103-currency-chf.png", icon: "103-currency-chfx.png" },
+        "CAD": { flag: "104-currency-cad.png", icon: "101-currency-cadx.png" },
+        "AUD": { flag: "105-currency-aud.png", icon: "104-currency-audx.png" },
+        "JPY": { flag: "113-currency-jpy.png", icon: "105-currency-jpyx.png" },
+        "KRW": { flag: "114-currency-krw.png", icon: "106-currency-krwx.png" },
+        "BRL": { flag: "108-currency-brl.png", icon: "107-currency-brlx.png" },
+        "MXN": { flag: "109-currency-mxn.png", icon: "108-currency-mxnx.png" },
+        "TRY": { flag: "106-currency-try.png", icon: "109-currency-tryx.png" },
+        "CNY": { flag: "107-currency-cny.png", icon: "110-currency-cnyx.png" },
+        "MYR": { flag: "117-currency-myr.png", icon: "111-currency-myrx.png" },
+        "RUB": { flag: "111-currency-rub.png", icon: "112-currency-rubx.png" },
+        "MAD": { flag: "118-currency-mad.png", icon: "113-currency-madx.png" },
+        "EGP": { flag: "119-currency-egp.png", icon: "114-currency-egbx.png" },
+        "TND": { flag: "120-currency-tnd.png", icon: "115-currency-tndx.png" },
+        "SAR": { flag: "121-currency-sar.png", icon: "116-currency-sarx.png" },
+        "QAR": { flag: "122-currency-qar.png", icon: "117-currency-qarx.png" },
+        "AED": { flag: "123-currency-aed.png", icon: "118-currency-aed.png" }
+    }
+};
