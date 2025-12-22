cd7007451f8e37fab140f75221085b68cfe1e5c9
jamalkatab69-cell <jamalkatab69@gmail.com>
2025-12-22 10:24:26 +0100

    Delete converter.js

--- converter.js
+++
@@ -1,7 +0,0 @@
-export const Converter = {
-    convert(amount, fromRate, toRate) {
-        // التحويل يعتمد على الدولار كأساس: (المبلغ / سعر المصدر) * سعر الهدف
-        if (!fromRate || !toRate) return 0;
-        return (amount / fromRate) * toRate;
-    }
-};
