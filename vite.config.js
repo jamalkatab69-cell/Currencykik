a8a8ad1feb24e79357e50207a76c8a219aee7b45
jamalkatab69-cell <jamalkatab69@gmail.com>
2025-12-24 11:48:57 +0100

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    base: './',
+    server: {
+        port: 3000,
+        open: true
+    },
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        sourcemap: false,
+        minify: 'terser',
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    'three': ['three']
+                }
+            }
+        }
+    },
+    optimizeDeps: {
+        include: ['three']
+    }
+});
